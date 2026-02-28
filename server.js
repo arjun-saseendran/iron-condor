75ac3ebb4a23f2ca84d7e13f1cc3eba9ceba7796
arjun-saseendran <your.arjunsaseendran@icloud.com>
2026-02-28 12:31:23 +0530

    fix port bug

--- server.js
+++ server.js
@@ -35,7 +35,7 @@ app.use('/api/auth/zerodha', authRoutes);
 >>>>>>> c3fd87d (fix path)
 app.use('/api/trades', tradeRoutes);  
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
 // ==========================================
 // 1-CLICK LOGIN REDIRECT
